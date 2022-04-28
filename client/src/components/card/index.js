a031a17ec8a45b353c0ec41e6d94c2c15925a922
Ahmed Tamer <69111538+atamerfahmy@users.noreply.github.com>
2022-04-28 06:07:00 +0200

    Update index.js

--- client/src/components/card/index.js
+++ client/src/components/card/index.js
@@ -34,10 +34,6 @@ const MultiActionAreaCard = ({ recipe, refresh }) => {
                          <Typography gutterBottom variant="h5" component="div">
                               {recipe.title}
                          </Typography>
-                         {/* <Typography variant="body2" color="text.secondary">
-                              Lizards are a widespread group of squamate reptiles, with over 6,000
-                              species, ranging across all continents except Antarctica
-                         </Typography> */}
                     </CardContent>
                </CardActionArea>
                <CardActions>
