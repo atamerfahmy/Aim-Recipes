56d8372786a128b0e2878b4355273580cece6167
Ahmed Tamer <69111538+atamerfahmy@users.noreply.github.com>
2022-04-28 05:51:49 +0200

    Update index.js

--- client/src/components/TopBar/index.js
+++ client/src/components/TopBar/index.js
@@ -4,9 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { useNavigate } from 'react-router-dom';
-// import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate, NavLink } from 'react-router-dom';
 
 export default function TopBar() {
      let navigate = useNavigate();
@@ -15,17 +13,10 @@ export default function TopBar() {
           <Box>
                <AppBar position="static">
                     <Toolbar>
-                         {/* <IconButton
-                              size="large"
-                              edge="start"
-                              color="inherit"
-                              aria-label="menu"
-                              sx={{ mr: 2 }}
-                         >
-                              <MenuIcon />
-                         </IconButton> */}
-                         <Typography onClick={() => navigate("/")} variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                              Aim-Recipes
+                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                              <NavLink to="/" style={{ textDecoration: 'none', color: "white" }}>
+                                   Aim-Recipes
+                              </NavLink>
                          </Typography>
                          <Button onClick={() => navigate("/add_recipe")} color="inherit">Add Recipe</Button>
                     </Toolbar>
